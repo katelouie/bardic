3b3641cf390ff598d4913feec1ef70980b37749b
Kate Louie <katehlouie@gmail.com>
2026-01-12 13:25:59 -0800

    Bump to 0.7.0

--- bardic/__init__.py
+++ bardic/__init__.py
@@ -1,6 +1,6 @@
 """Bardic: Python-first interactive fiction engine"""
 
-__version__ = "0.6.1"
+__version__ = "0.7.0"
 
 from .compiler.compiler import BardCompiler
 from .compiler.parser import parse, parse_file
